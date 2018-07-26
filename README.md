d57d98fdbf2211e7e081350e5a5a83a350c01e15
Lucas Holmquist <lholmqui@redhat.com>
2018-07-26 13:55:11 +0000

    feat: update the booster to conform to how the other node boosters are setup.
    * adding the .openshiftio directory
    * add nodeshift
    * add the license-reporter

--- README.md
+++ README.md
@@ -18,30 +18,10 @@ message protocol to send and receive messages.
 Run the following commands to configure and deploy the applications.
 
 ```bash
-oc apply -f templates
+$ oc create -f service.amqp.yaml
 
-oc new-app --template=amq63-basic \
-  -p APPLICATION_NAME=work-queue-broker \
-  -p IMAGE_STREAM_NAMESPACE=$(oc project -q) \
-  -p MQ_PROTOCOL=amqp \
-  -p MQ_QUEUES=work-queue/requests,work-queue/responses \
-  -p MQ_TOPICS=work-queue/worker-updates \
-  -p MQ_USERNAME=work-queue \
-  -p MQ_PASSWORD=work-queue
-
-oc new-app --template=nodejs-messaging-work-queue-frontend
-oc new-app --template=nodejs-messaging-work-queue-worker
+$ ./start-openshift
 ```
-
-<!-- oc new-app --template=amq-broker-71-basic \ -->
-<!--   -p APPLICATION_NAME=work-queue-broker \ -->
-<!--   -p IMAGE_STREAM_NAMESPACE=$(oc project -q) \ -->
-<!--   -p AMQ_PROTOCOL=amqp \ -->
-<!--   -p AMQ_QUEUES=work-queue/requests \ -->
-<!--   -p AMQ_ADDRESSES=work-queue/worker-updates \ -->
-<!--   -p AMQ_USER=work-queue \ -->
-<!--   -p AMQ_PASSWORD=work-queue -->
-
 ## Modules
 
 The `frontend` module serves the web interface and communicates with
---
+++ worker/app.js
@@ -0,0 +1,135 @@
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// 'License'); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+'use strict';
+
+const path = require('path');
+const crypto = require('crypto');
+const express = require('express');
+const probe = require('kube-probe');
+const rhea = require('rhea');
+
+const amqpHost = process.env.MESSAGING_SERVICE_HOST || 'localhost';
+const amqpPort = process.env.MESSAGING_SERVICE_PORT || 5672;
+const amqpUser = process.env.MESSAGING_SERVICE_USER || 'work-queue';
+const amqpPassword = process.env.MESSAGING_SERVICE_PASSWORD || 'work-queue';
+
+// AMQP
+
+const id = 'worker-nodejs-' + crypto.randomBytes(2).toString('hex');
+const container = rhea.create_container({id});
+
+let workerUpdateSender = null;
+let requestsProcessed = 0;
+let processingErrors = 0;
+
+function processRequest (request) {
+  const uppercase = request.application_properties.uppercase;
+  const reverse = request.application_properties.reverse;
+  let text = request.body;
+
+  console.log('request text', text);
+
+  if (uppercase) {
+    text = text.toUpperCase();
+  }
+
+  if (reverse) {
+    text = text.split("").reverse().join("");
+  }
+
+  return text;
+}
+
+container.on('connection_open', event => {
+  console.log(`${id}: Connected to AMQP messaging service at ${amqpHost}:${amqpPort}`);
+
+  event.connection.open_receiver('work-queue/requests');
+  workerUpdateSender = event.connection.open_sender('work-queue/worker-updates');
+});
+
+container.on('message', event => {
+  const request = event.message;
+  let responseBody;
+
+  console.log('%s: Received request %s', id, request);
+
+  try {
+    responseBody = processRequest(request);
+  } catch (e) {
+    console.error('%s: Failed processing message: %s', id, e);
+    processingErrors++;
+    return;
+  }
+
+  const response = {
+    to: request.reply_to,
+    correlation_id: request.message_id,
+    application_properties: {
+      workerId: container.id
+    },
+    body: responseBody
+  };
+
+  event.connection.send(response);
+
+  requestsProcessed++;
+
+  console.log('%s: Sent response %s', id, JSON.stringify(response));
+});
+
+function sendUpdate () {
+  if (!workerUpdateSender || !workerUpdateSender.sendable()) {
+    return;
+  }
+
+  const update = {
+    application_properties: {
+      workerId: container.id,
+      timestamp: new Date().getTime(),
+      requestsProcessed,
+      processingErrors
+    }
+  };
+
+  workerUpdateSender.send(update);
+}
+
+setInterval(sendUpdate, 5 * 1000);
+
+const opts = {
+  host: amqpHost,
+  port: amqpPort,
+  username: amqpUser,
+  password: amqpPassword
+};
+
+console.log(`${id}: Attempting to connect to AMQP messaging service at ${amqpHost}:${amqpPort}`);
+container.connect(opts);
+
+// HTTP
+
+const app = express();
+
+// Expose the license.html at http[s]://[host]:[port]/licences/licenses.html
+app.use('/licenses', express.static(path.join(__dirname, 'licenses')));
+
+probe(app);
+
+module.exports = app;
